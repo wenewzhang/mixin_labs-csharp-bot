f0d9430fd109a3f7e786fdf182fced8b269efd24
wenewzhang <wenewboy@gmail.com>
2019-03-07 16:59:29 +0800

    Write private key to text file

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ using Org.BouncyCastle.Crypto.Generators;
 using Org.BouncyCastle.Crypto.Prng;
 using Org.BouncyCastle.Security;
 using Org.BouncyCastle.X509;
+using System.Security.Cryptography;
+using Org.BouncyCastle.Crypto.Parameters;
 
 using System.IO;
 using System.Text;
@@ -43,50 +45,164 @@ MTvukq+k3M9xkAhWuvXAEOUcxrFYE0vPWQIJUzYwNqk=
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             MixinApi mixinApi = new MixinApi();
             mixinApi.Init(ClientId,
                           ClientSecret,
                           SessionId,
                           PinToken,
                           PrivateKey);
-            Console.WriteLine(mixinApi.VerifyPIN(PinCode).ToString());
+            string PromptMsg;
+            PromptMsg  = "1: Create user and update PIN\n2: Read Bitcoin balance \n3: Read Bitcoin Address\n4: Read EOS balance\n";
+          	PromptMsg += "5: Read EOS address\n6: Transfer Bitcoin from bot to new account\n7: Transfer Bitcoin from new account to Master\n";
+          	PromptMsg += "8: Withdraw bot's Bitcoin\na: Verify Pin\nd: Create Address and Delete it\nr: Create Address and read it\n";
+          	PromptMsg += "q: Exit \nMake your choose:";
+            // Console.WriteLine(mixinApi.VerifyPIN(PinCode).ToString());
+            do {
+            Console.WriteLine(PromptMsg);
+            var cmd = Console.ReadLine();
+            if (cmd == "1") {
+              var kpgen = new RsaKeyPairGenerator();
 
-            // var kpgen = new RsaKeyPairGenerator();
-            //
-            // kpgen.Init(new KeyGenerationParameters(new SecureRandom(new CryptoApiRandomGenerator()), 1024));
-            //
-            // var keyPair = kpgen.GenerateKeyPair();
-            // AsymmetricKeyParameter privateKey = keyPair.Private;
-            // AsymmetricKeyParameter publicKey = keyPair.Public;
-            //
-            // SubjectPublicKeyInfo info = SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(keyPair.Public);
-            // string pk = Convert.ToBase64String(info.GetDerEncoded());
-            //
-            //
-            // var user = mixinApi.APPUser("Csharp" + (new Random().Next() % 100) + " Cat", pk);
-            // Console.WriteLine(user);
-            var u = mixinApi.SearchUser("37222956");
-            Console.WriteLine(u);
-            Console.WriteLine(u.user_id);
-            Console.WriteLine(u.full_name);
+              kpgen.Init(new KeyGenerationParameters(new SecureRandom(new CryptoApiRandomGenerator()), 1024));
 
-            using (var writer = new StreamWriter("new_users.csv"))
-            using (var csv = new CsvWriter(writer))
-            {
-                csv.WriteField(u.user_id);
-                csv.WriteField(u.full_name);
-                csv.NextRecord();
-                csv.Flush();
+              var keyPair = kpgen.GenerateKeyPair();
+              AsymmetricKeyParameter privateKey = keyPair.Private;
+              AsymmetricKeyParameter publicKey = keyPair.Public;
+
+              SubjectPublicKeyInfo info = SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(keyPair.Public);
+              string pk = Convert.ToBase64String(info.GetDerEncoded());
+
+
+              var user = mixinApi.APPUser("Csharp" + (new Random().Next() % 100) + " Cat", pk);
+              Console.WriteLine(user);
+              using (var writer = new StreamWriter("new_users.csv"))
+              using (var csv = new CsvWriter(writer))
+              {
+                  csv.WriteField(user.user_id);
+
+                  RsaPrivateCrtKeyParameters rsaParameters = (RsaPrivateCrtKeyParameters) privateKey;
+
+                  RSACryptoServiceProvider priKey = new RSACryptoServiceProvider();
+                  priKey.ImportParameters(DotNetUtilities.ToRSAParameters(rsaParameters));
+
+                  // csv.WriteField(DotNetUtilities.ToRSA(keyPair.Private));
+                  csv.WriteField(user.pin_token);
+                  csv.WriteField(user.session_id);
+                  csv.NextRecord();
+                  csv.Flush();
+              }
             }
+            if (cmd == "q") { break; }
+            if (cmd == "s") {
+              var u = mixinApi.SearchUser("37222956");
+              Console.WriteLine(u);
+              Console.WriteLine(u.user_id);
+              Console.WriteLine(u.full_name);
 
-            // using (var reader = new StreamReader(file.InputStream))
-            // using (var csvReader = new CsvReader(reader))
-            // {
-            //     // Use While(csvReader.Read()); if you want to read all the rows in the records)
-            //     csvReader.Read();
-            //     return csvReader.GetRecords<SurveyEmailListModels>().ToArray();
-            // }
+              using (var writer = new StreamWriter("new_users.csv"))
+              using (var csv = new CsvWriter(writer))
+              {
+                  csv.WriteField(u.user_id);
+                  csv.WriteField(u.full_name);
+                  csv.NextRecord();
+                  csv.Flush();
+              }
+            }
+          } while (true);
+        }
+        private static void ExportPrivateKey(RSACryptoServiceProvider csp, TextWriter outputStream)
+        {
+            if (csp.PublicOnly) throw new ArgumentException("CSP does not contain a private key", "csp");
+            var parameters = csp.ExportParameters(true);
+            using (var stream = new MemoryStream())
+            {
+                var writer = new BinaryWriter(stream);
+                writer.Write((byte)0x30); // SEQUENCE
+                using (var innerStream = new MemoryStream())
+                {
+                    var innerWriter = new BinaryWriter(innerStream);
+                    EncodeIntegerBigEndian(innerWriter, new byte[] { 0x00 }); // Version
+                    EncodeIntegerBigEndian(innerWriter, parameters.Modulus);
+                    EncodeIntegerBigEndian(innerWriter, parameters.Exponent);
+                    EncodeIntegerBigEndian(innerWriter, parameters.D);
+                    EncodeIntegerBigEndian(innerWriter, parameters.P);
+                    EncodeIntegerBigEndian(innerWriter, parameters.Q);
+                    EncodeIntegerBigEndian(innerWriter, parameters.DP);
+                    EncodeIntegerBigEndian(innerWriter, parameters.DQ);
+                    EncodeIntegerBigEndian(innerWriter, parameters.InverseQ);
+                    var length = (int)innerStream.Length;
+                    EncodeLength(writer, length);
+                    writer.Write(innerStream.GetBuffer(), 0, length);
+                }
+
+                var base64 = Convert.ToBase64String(stream.GetBuffer(), 0, (int)stream.Length).ToCharArray();
+                outputStream.WriteLine("-----BEGIN RSA PRIVATE KEY-----");
+                // Output as Base64 with lines chopped at 64 characters
+                for (var i = 0; i < base64.Length; i += 64)
+                {
+                    outputStream.WriteLine(base64, i, Math.Min(64, base64.Length - i));
+                }
+                outputStream.WriteLine("-----END RSA PRIVATE KEY-----");
+            }
+        }
+
+        private static void EncodeLength(BinaryWriter stream, int length)
+        {
+            if (length < 0) throw new ArgumentOutOfRangeException("length", "Length must be non-negative");
+            if (length < 0x80)
+            {
+                // Short form
+                stream.Write((byte)length);
+            }
+            else
+            {
+                // Long form
+                var temp = length;
+                var bytesRequired = 0;
+                while (temp > 0)
+                {
+                    temp >>= 8;
+                    bytesRequired++;
+                }
+                stream.Write((byte)(bytesRequired | 0x80));
+                for (var i = bytesRequired - 1; i >= 0; i--)
+                {
+                    stream.Write((byte)(length >> (8 * i) & 0xff));
+                }
+            }
+        }
+
+        private static void EncodeIntegerBigEndian(BinaryWriter stream, byte[] value, bool forceUnsigned = true)
+        {
+            stream.Write((byte)0x02); // INTEGER
+            var prefixZeros = 0;
+            for (var i = 0; i < value.Length; i++)
+            {
+                if (value[i] != 0) break;
+                prefixZeros++;
+            }
+            if (value.Length - prefixZeros == 0)
+            {
+                EncodeLength(stream, 1);
+                stream.Write((byte)0);
+            }
+            else
+            {
+                if (forceUnsigned && value[prefixZeros] > 0x7f)
+                {
+                    // Add a prefix zero to force unsigned if the MSB is 1
+                    EncodeLength(stream, value.Length - prefixZeros + 1);
+                    stream.Write((byte)0);
+                }
+                else
+                {
+                    EncodeLength(stream, value.Length - prefixZeros);
+                }
+                for (var i = prefixZeros; i < value.Length; i++)
+                {
+                    stream.Write(value[i]);
+                }
+            }
         }
     }
 }
