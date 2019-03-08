1f77a5f0b84d58f3e4f4d47090770774e0c68118
wenewzhang <wenewboy@gmail.com>
2019-03-08 16:06:39 +0800

    rearrange directory

---
+++ echo_bot/config.cs
@@ -0,0 +1,28 @@
+
+namespace echo_bot
+{
+    static class USRCONFIG
+    {
+        public static string ClientId = "daf8b473-39a0-4419-991a-77f30d28dd6d";
+        public static string ClientSecret = "d83083de5a3683f80807f347776de46b2980e87d8145dbf3a3aef0cd5d698a70";
+        public static string PinCode = "485371";
+        public static string SessionId = "ec0550ac-bcc4-4da6-82d1-4c553b3e32c6";
+        public static string PinToken = "OxoXiP0X4DLDBjP+rz9wNvQSl7+BQ1LLxKo/LYPRfmAkiNJH2jUA4yt178OlRmJC8+Xt1Mt0rwMYbRDQlV6X8yHvZC5vqaHjaJxbzGoJPJ0/lIx0dJtkn3kEyz1jOhYHsdJbd9VZALBB/opTNlC3vC63DcJnHeOkE58iPaBJCoY=";
+        public static string PrivateKey =
+        @"-----BEGIN RSA PRIVATE KEY-----
+MIICXgIBAAKBgQCfjQzTqsuAEtUOT1fPqh9Xc52L3G1YN2bE7JLFt9PG6k3Hanal
+ZHTjAzD5sr9A8wivU48S5ryWaGU9275wZYnuF4D4ytzW8lj+Osc5jisL1GS/AZb4
+RuV1EMZuG6RirXjWc3dvXrCNGpijDyxtf78B264AHJfGA2xmDL/iDJBCbwIDAQAB
+AoGAEYxJ5N3cE/H+TNydI9HqMIwev8s2m4U8YFxEqEpXmIEcnNowwnASCwXG1S+7
+qUERq3bdPY7zHvfMaslIYMRKLyCkhETtiBjZiFJw84ANUN93i9cM/W6EN/KN5TNE
+Fnb4OKjzduSJ//9BmoCf+CV9oeq4zWUcdR5xhl5Z6x45AsECQQDqZbkhnNZN2ofv
+kB+Pa1+txDAJqrEoZ7j6Axlq3ikHLr//7/XbRytwtNoQNTU2LEtlRDaxxemim2Gd
+UvWgCcpNAkEArkFufbbfvbk8ML78+nUyqHWJnq1TfwlL5tJl+yuZAhwSbo1yaPS6
+RGYjRQ7GcSVYHoVX1wHMpeht2fasfW0lqwJBAKc46SncvVsPk7lKRenIvZq3g1ny
+J69eyC29Nk3yrqyMIrgR1oGYtZoTvBqiCxNZ6HQXhTmwq2yxk6NsgwWo1mUCQQCI
+QTeRa2aGJopvH1gWy39iBA4pGiSYx/8mrMv7WzrYUHUFJusYMIxZoXfgrs3yeR3v
+N5rYz1pjPFxxO5XiuW7jAkEAsLVHubu2ldvy89aNwA/ciCe7C43KwiVprPvIari0
+LS7+JeZlHrGiQoqmu/0YBVfIkPxMpeYOuRgxjCwdq0xvfQ==
+-----END RSA PRIVATE KEY-----";
+    }
+}
