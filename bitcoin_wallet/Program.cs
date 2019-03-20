b1c5f465d50e7613cae0c1adbe0e62be632d0746
wenewzhang <wenewboy@gmail.com>
2019-03-20 17:22:47 +0800

    815b0b1a-2764-3736-8faa-42d694fa620a -> xBAaC1uBZCc2N4+qQtaU+mIK

--- bitcoin_wallet/Program.cs
+++ bitcoin_wallet/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Text;
 using ProGaudi.MsgPack.Light;
-// using MsgPack.Serialization;
+// using MessagePack;
 namespace bitcoint_wallet
 {
     class Program
@@ -8,12 +9,20 @@ namespace bitcoint_wallet
         static void Main(string[] args)
         {
           // "815b0b1a-2764-3736-8faa-42d694fa620a"
-          // gaFBxBCBWwsaJ2Q3No+qQtaU+mIK
+          // php: gaFBxBCBWwsaJ2Q3No+qQtaU+mIK
+          // c#:  xBAaC1uBZCc2N4+qQtaU+mIK
             Console.WriteLine("Hello World!");
-            var bytes = MsgPackSerializer.Serialize("815b0b1a-2764-3736-8faa-42d694fa620a");
+            Guid guid = new Guid("815b0b1a-2764-3736-8faa-42d694fa620a");
+            var gbytes = guid.ToByteArray();
+            foreach (var byt in gbytes)
+            Console.Write("{0:X2} ", byt);
+            Console.WriteLine("");
+            var bytes = MsgPackSerializer.Serialize(gbytes);
+            // var bytes = MessagePackSerializer.Serialize(Guid.NewGuid().ToByteArray());
             Console.WriteLine(Convert.ToBase64String(bytes));
-            var value = MsgPackSerializer.Deserialize<string>(bytes);
-            Console.WriteLine(value);
+            // var value = MsgPackSerializer.Deserialize<string>(bytes);
+            // var value = MessagePackSerializer.Deserialize<string>(bytes);
+            // Console.WriteLine(value);
         }
     }
 }
