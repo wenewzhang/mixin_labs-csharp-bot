b2f170629d6286ad38f88a82a9238114dc861af0
wenewzhang <wenewboy@gmail.com>
2019-03-20 16:21:07 +0800

    msgpack demo

---
+++ bitcoin_wallet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ProGaudi.MsgPack.Light;
+// using MsgPack.Serialization;
+namespace bitcoint_wallet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          // "815b0b1a-2764-3736-8faa-42d694fa620a"
+          // gaFBxBCBWwsaJ2Q3No+qQtaU+mIK
+            Console.WriteLine("Hello World!");
+            var bytes = MsgPackSerializer.Serialize("815b0b1a-2764-3736-8faa-42d694fa620a");
+            Console.WriteLine(Convert.ToBase64String(bytes));
+            var value = MsgPackSerializer.Deserialize<string>(bytes);
+            Console.WriteLine(value);
+        }
+    }
+}
