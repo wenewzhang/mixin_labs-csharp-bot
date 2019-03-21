2f319d190d46d02174c4e246fac8ce4aa33031b9
wenewzhang <wenewboy@gmail.com>
2019-03-21 16:46:20 +0800

    MsgPack compatible with PHP&GO

--- bitcoin_wallet/Program.cs
+++ bitcoin_wallet/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Text;
-using ProGaudi.MsgPack.Light;
-// using MessagePack;
+using SimpleMsgPack;
 namespace bitcoint_wallet
 {
     class Program
@@ -10,19 +9,25 @@ namespace bitcoint_wallet
         {
           // "815b0b1a-2764-3736-8faa-42d694fa620a"
           // php: gaFBxBCBWwsaJ2Q3No+qQtaU+mIK
-          // c#:  xBAaC1uBZCc2N4+qQtaU+mIK
-            Console.WriteLine("Hello World!");
-            Guid guid = new Guid("815b0b1a-2764-3736-8faa-42d694fa620a");
+          // c#:  xBCBWwsaJ2Q3No+qQtaU+mIK
+
+         // "815b0b1a-2764-3736-8faa-42d694fa620a"
+          //php: gaFBxBDG0McoJiRCm44N2dGbZZL6
+          // C#: xBDG0McoJiRCm44N2dGbZZL6
+            Console.WriteLine("c6d0c728-2624-429b-8e0d-d9d19b6592fa");
+            Guid guid = new Guid("c6d0c728-2624-429b-8e0d-d9d19b6592fa");
             var gbytes = guid.ToByteArray();
+            Array.Reverse(gbytes,0,4);
+            Array.Reverse(gbytes,4,2);
+            Array.Reverse(gbytes,6,2);
             foreach (var byt in gbytes)
             Console.Write("{0:X2} ", byt);
-            Console.WriteLine("");
-            var bytes = MsgPackSerializer.Serialize(gbytes);
-            // var bytes = MessagePackSerializer.Serialize(Guid.NewGuid().ToByteArray());
-            Console.WriteLine(Convert.ToBase64String(bytes));
-            // var value = MsgPackSerializer.Deserialize<string>(bytes);
-            // var value = MessagePackSerializer.Deserialize<string>(bytes);
-            // Console.WriteLine(value);
+
+            MsgPack msgpack = new MsgPack();
+            msgpack.SetAsBytes(gbytes);
+
+            byte[] packData = msgpack.Encode2Bytes();
+            Console.WriteLine(Convert.ToBase64String(packData));
         }
     }
 }
